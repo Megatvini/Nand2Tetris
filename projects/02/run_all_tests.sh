39995ea315a8339ddcf82f2ecae6b09a39a7e1dd
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2017-03-28 18:14:55 +0400

    Write Project 2

--- projects/02/run_all_tests.sh
+++ projects/02/run_all_tests.sh
@@ -2,6 +2,6 @@ for directory in ${path}*.tst
 do
     if [ -f "$directory" ] ; then  #test to see if it is a file and then executes the following commands.
 		echo "$directory"
-	    /mnt/c/Users/dogho/Desktop/Nand2Tetris/tools/HardwareSimulator.sh "$directory"
+	    ~/Desktop/Nand2Tetris/tools/HardwareSimulator.sh "$directory"
     fi
 done
