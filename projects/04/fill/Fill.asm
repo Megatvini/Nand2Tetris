b4ce502da53312fc3b0855f56fefeae8c8861225
megatvini <ndogh13@freeuni.edu.ge>
2017-04-16 16:04:44 +0400

    implement project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,87 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+	@pixel
+	M=0		//make pixel value 0
+	
+	@KBD
+	D=M     //D = keyboard input
+	
+	@NOINPUT
+	D;JEQ   //if input is zero do nothing
+	
+	@pixel
+	M=-1	//input is not zero so make pixel black
+	
+	(NOINPUT)
+	
+	@i
+	M=0     //i = 0
+	
+	(FOR_I)
+		@i
+		D=M
+		
+		@8192
+		D=D-A //D = i-8192
+	
+		@END_FOR_I
+		D;JGE    //if (i - 8192) >= 0 end END_FOR_I
+		
+		@j
+		M=0  //j = 0
+		
+		(FOR_J)
+			@j
+			D=M
+			
+			@32
+			D=D-A  //D = j - 32
+			
+			@END_FOR_J
+			D;JGE  // if (j - 32 >= 0) goto END_FOR_J
+			
+			@i
+			D=M    //D = i
+			
+			@j
+			D=D+M  //D = i + j
+			
+			@SCREEN
+			D=D+A  //D = SCREEN + i + j
+			
+			@R0
+			M=D    //R0 = D
+			
+			@pixel
+			D=M   //D = pixel
+			
+			@R0
+			A=M   //A = R0
+			
+			M=D   //SCREEN[R0] = D
+			
+			@j
+			M=M+1 //j++
+			
+		@FOR_J
+		0;JMP	//goto second for loop
+		
+		(END_FOR_J)
+		
+		@i
+		D=M
+		@32
+		D=D+A	
+		@i
+		M=D //i = i + 32
+		
+	@FOR_I
+	0;JMP	//goto first for loop
+	
+	(END_FOR_I)
+	
+@LOOP	//start loop again
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@res 	// multiplication result
+	M=0  	// res = 0
+
+	@i
+	M=0 	//i = 0
+
+(LOOP)
+	@i
+	D=M 	//D = i
+	
+	@R0
+	D=D-M	//D = i - R0
+	
+	@END
+	D;JGE // if (i - R0) >= 0 jump to END
+	
+	@R1
+	D=M 	//D = R1
+	
+	@res
+	M=M+D 	//res += R1
+	
+	@i
+	M=M+1	//i++
+	
+	@LOOP
+	0; JMP  //goto LOOP
+
+(END)
+	@res
+	D=M     //D = res
+	
+	@R2
+	M=D    //R2 = res
+	
+
+(INFINITE)
+	@INFINITE
+	0;JMP 	//Infinite LOOP
